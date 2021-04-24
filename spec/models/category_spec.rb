e43fcfd00b06da690be8048c66c124a194772b02
Kobiyoyo <adamachubi@gmail.com>
2021-04-24 22:19:57 +0100

    done with backend

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'association test' do
+    it { is_expected.to have_many(:products) }
+  end
+
+  describe 'validation' do
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_length_of(:name).is_at_least(4) }
+    it { is_expected.to validate_presence_of :category_code }
+  end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'association test' do
+    it { is_expected.to belong_to(:category) }
+  end
+
+  describe 'validation' do
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_length_of(:name).is_at_least(4) }
+    it { is_expected.to validate_presence_of :brand }
+    it { is_expected.to validate_length_of(:brand).is_at_least(4) }
+    it { is_expected.to validate_presence_of :product_code }
+    it { is_expected.to validate_presence_of :quantity }
+    it { is_expected.to validate_presence_of :price }
+  end
 end
+
